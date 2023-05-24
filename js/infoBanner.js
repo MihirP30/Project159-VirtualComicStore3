a115a39df7223a9129943029cf9bc0381dcd0b5e
MihirP30 <71361209+MihirP30@users.noreply.github.com>
2023-05-23 22:29:29 -0400

    Add files via upload

---
+++ js/infoBanner.js
@@ -0,0 +1,111 @@
+AFRAME.registerComponent("info-banner", {
+  schema: {
+    itemId: { default: "", type: "string" },
+  },
+  update: function () {
+    this.createBanner();
+  },
+  createBanner: function () {
+    postersInfo = {
+      "one-piece": {
+        banner_url: "./assets/banners/onepiece.jpg",
+        title: "One Piece",
+        released_year: "1997",
+        description:
+          "Monkey. D. Luffy refuses to let anyone or anything stand in the way of his quest to become the king of all pirates. With a course charted for the treacherous waters of the Grand Line and beyond, this is one captain who'll never give up until he's claimed the greatest treasure on Earth: the Legendary One Piece!",
+      },
+      "bleach": {
+        banner_url: "./assets/banners/bleach.jpg",
+        title: "Bleach",
+        released_year: "2004",
+        description:
+          "Ichigo Kurosaki never asked for the ability to see ghosts -- he was born with the gift. When his family is attacked by a Hollow -- a malevolent lost soul -- Ichigo becomes a Soul Reaper, dedicating his life to protecting the innocent and helping the tortured spirits themselves find peace.",
+      },
+      "naruto": {
+        banner_url: "./assets/banners/naruto.jpg",
+        title: "Naruto",
+        released_year: "2002",
+        description:
+          "The Village Hidden in the Leaves is home to the stealthiest ninja.  But twelve years earlier, a fearsome Nine-tailed Fox terrorized the village before it was subdued and its spirit sealed within the body of a baby boy.",
+      },
+      "fairy-tail": {
+        banner_url: "./assets/banners/fairytail.jpg",
+        title: "Fairy Tail",
+        released_year: "2009",
+        description:
+          "The story follows a teenage girl named Lucy Heartfilla who is determined to join the notorious magical Fairy Tail Guild. During a daring rescue, she encounters Natsu who is part of the guild and eventually offers her a place. They become teammates performing various missions for the Fairy Tail Guild. Be prepared for an action-packed adventure!",
+      },
+    };
+    const { itemId } = this.data;
+
+    const fadeBackgroundEl = document.querySelector("#fadeBackground");
+
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("id", `${itemId}-banner`);
+
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.9,
+      height: 1,
+    });
+
+    entityEl.setAttribute("material", { color: "#000" });
+    entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+
+    const item = postersInfo[itemId];
+
+    const imageEl = this.createImageEl(item);
+    const titleEl = this.createTitleEl(item);
+    const descriptionEl = this.createDescriptionEl(item);
+
+    entityEl.appendChild(imageEl);
+    entityEl.appendChild(titleEl);
+    entityEl.appendChild(descriptionEl);
+
+    fadeBackgroundEl.appendChild(entityEl);
+  },
+  createImageEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.85,
+      height: 0.4,
+    });
+    entityEl.setAttribute("material", { src: item.banner_url });
+    entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+    return entityEl;
+  },
+  createTitleEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 1.2,
+      height: 2,
+      color: "#fff",
+      value: `${item.title} (${item.released_year})`,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+    return entityEl;
+  },
+  createDescriptionEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 0.75,
+      height: 2,
+      color: "#fff",
+      wrapCount: "40",
+      value: item.description,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+    return entityEl;
+  },
+});
